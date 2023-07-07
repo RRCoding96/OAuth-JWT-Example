22f5c499084ba1f0bd7c4f03ee58ec90c98c068a
김윤욱 <reznov03048@gmail.com>
2023-07-07 23:23:42 +0900

    feat: OpenFeign 테스트용 컨트롤러

---
+++ src/main/java/com/example/oauth/api/feigntest/client/HelloClient.java
@@ -0,0 +1,13 @@
+package com.example.oauth.api.feigntest.client;
+
+import com.example.oauth.api.health.dto.HealthCheckResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(url = "http://localhost:8080", name = "helloClient")
+public interface HelloClient {
+
+    @GetMapping(value = "/api/health", consumes = "application/json")
+    HealthCheckResponseDto healthCheck();
+
+}
